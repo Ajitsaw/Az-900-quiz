ca4bfdc7bca06ab8743de2965a08eeb58c87541a
Priyanka Shaw <shaw.ajit02@gmail.com>
2025-09-28 00:05:45 +0530

    Add deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
   },
-};
+  reactStrictMode: true,
+  // Base path for GitHub Pages
+  basePath: process.env.NODE_ENV === 'production' ? '/az-900' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/az-900/' : '',
+}
 
-export default nextConfig;
+export default nextConfig
